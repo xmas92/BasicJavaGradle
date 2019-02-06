75e7bf480a63ea7b3d3e35027ce9cffef46a292b
Axel Boldt-Christmas <xmas1915@gmail.com>
2019-02-06 21:18:27 +0100

    It can run, and Add

---
+++ java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static int Add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println("2 + 2 = " + Add(2,2));
+    }
+}
---
+++ test/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testAdd() {
+        assertEquals(Main.Add(2,2), 4);
+    }
+}
