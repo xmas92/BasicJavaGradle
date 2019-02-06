19876041d3b9ae6524d586d7436a773726162a99
Axel Boldt-Christmas <xmas1915@gmail.com>
2019-02-06 22:31:57 +0100

    Everyone knows semis are for losers.

--- java/Main.java
+++ java/Main.java
@@ -3,6 +3,6 @@ public class Main {
         return a + b;
     }
     public static void main(String[] args) {
-        System.out.println("2 + 2 = " + Add(2,2));
+        System.out.println("2 + 2 = " + Add(2,2))
     }
 }
